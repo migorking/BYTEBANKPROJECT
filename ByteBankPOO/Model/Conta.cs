95f9d6fa4963cc50736fa25ea076e7c950bd3d5b
Mario Igor Barbosa <84343722+migorking@users.noreply.github.com>
2023-01-06 19:10:14 -0300

    Adding project BYTEBANK
    Só tem um commit, pq fiz em outro repositório, porem depois do HUGO ensinar o POO, comecei um projeto novo do zero!

---
+++ ByteBankPOO/Model/Conta.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBankPOO.Model
+{
+    public class Conta
+    {
+
+        public decimal Saldo { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public string Senha { get; set; }
+
+        public Conta(string nome, string cpf, string senha, decimal saldo)
+        {
+
+            Saldo = saldo;
+            Nome = nome;
+            Cpf = cpf;
+            Senha = senha;
+        }
+      
+
+
+
+        public decimal VerSaldo()
+        {
+            return Saldo;
+        }
+
+        public void DepositarDinheiro(decimal valorDeposito)
+        {
+            Saldo += valorDeposito;
+        }
+
+        public void SacarDinheiro(decimal totalSaque)
+        {
+            if (Saldo < totalSaque)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\nNÃO A SALDO SUFICIENTE");
+                Console.ResetColor();
+            }
+            else
+            {
+                Saldo -= totalSaque;
+            }
+        }
+
+        public void TransferirDinheiro(Conta contaEnvio, Conta contaDestino, decimal valorTransf)
+        {
+            contaEnvio.Saldo -= valorTransf;
+            contaDestino.Saldo += valorTransf;
+        }
+
+
+
+
+
+    }
+}
